14f30fbe935fae23b5e79716a0f079d4b00f2617
Mario Diosdado <diosdado.mario@gmail.com>
2020-11-29 09:37:02 -0600

    Search and sort by name is now working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import TableData from "./components/TableData";
-import SearchBar from "./components/SearchBar";
+// import SearchBar from "./components/SearchBar";
+
+
 
 function App() {
+  
+
   return <>
   <Header />
-  <SearchBar />
   <TableData/>
   </>
 }
--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,27 +1,69 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import employees from "../employees.json";
 
 
 
-class TableData extends Component {
-    state = {
-        employees
-    };
+function TableData() {
 
 
-    render() {
-        return (
+    const [data, setData] = useState("");
+    const [sortData, setSortData] = useState(false);
+
+    function search(employees) {
+        return employees.filter(
+            (employee) =>
+                employee.name.toLowerCase().indexOf(data.toLowerCase()) > - 1 ||
+                employee.occupation.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+                employee.email.toLowerCase().indexOf(data.toLowerCase()) > -1
+        );
+    }
+
+    const dataTable = search(employees);
+
+
+    function handleSort(a, b) {
+        if (!sortData) {
+            let compare = 0;
+            if (a.name > b.name) {
+                compare = 1;
+            } else if (a.name < b.name) {
+                compare = -1
+            }
+            setSortData(true);
+            return (
+                compare
+            );
+        } else {
+            let compare = 0;
+            if (a.name > b.name) {
+                compare = -1;
+            } else if (a.name < b.nam) {
+                compare = 1
+            }
+            setSortData(false);
+            return (
+                compare
+            );
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" value={data} onChange={(event) => setData(event.target.value)} />
+            </div>
             <table className="table">
                 <thead>
                     <tr>
                         <th>Id</th>
-                        <th> <button onClick={() => sortf("name")}>Name</button></th>
+                        <th> <button onClick={() => employees.sort(handleSort)}>Name</button></th>
                         <th>Occupation</th>
                         <th>Email</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {employees.map(employee => (
+                    {dataTable.map(employee => (
                         <tr key={employee.id}>
                             <th scope="row">{employee.id}</th>
                             <td>{employee.name}</td>
@@ -31,24 +73,12 @@ class TableData extends Component {
                     ))}
                 </tbody>
             </table>
-        )
-    }
+        </div>
+    )
+
 }
-function sortf(props) {
-    // const {employees} =  props;
-    // let sortedEmployees = [...employees];
-    // sortedEmployees.sort((a, b) => {
-    //     if(a.name < b.name) {
-    //         return -1;
-    //     }
-    //     if(a.name > b.name) {
-    //         return 1;
-    //     }
-    //     return 0
-    // });
-    console.log(employees);
-    };
-    
+
+
 
 
 
